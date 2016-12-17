ba9c5d8832027e8f99f73be395f0e10502cb77c4
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-12-17 16:56:45 +0300

    [Chore] Refactor consumer

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -2,7 +2,7 @@ const Kafka = require('no-kafka');
 
 const consumer = new Kafka.SimpleConsumer
 // data handler function can return a Promise
-var dataHandler = function (messageSet, topic, partition) {
+var data = function (messageSet, topic, partition) {
     messageSet.forEach(function (m) {
       // console.log(m.message.value);
         console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
@@ -11,5 +11,5 @@ var dataHandler = function (messageSet, topic, partition) {
 
 return consumer.init().then(function () {
     // Subscribe partitons 0 and 1 in a topic:
-    return consumer.subscribe('kafka-python-topic', [0] , dataHandler);
+    return consumer.subscribe('kafka-python-topic', data);
 });
